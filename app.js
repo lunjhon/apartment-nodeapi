584b8e3f40670590f2f5e6770e53b311ae4d121c
unknown <jerrycj11@hotmail.com>
2021-04-01 22:01:51 +0500

    Fix Access to XMLHttpRequest

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var createError = require('http-errors');
+var cors = require('cors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -18,7 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
+app.options('*', cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
